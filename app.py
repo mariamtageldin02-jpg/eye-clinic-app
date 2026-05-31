a259cab7f9ccce940e491be2f3b2eef2be783be3
mariamtageldin02-jpg <mariam.tageldin.02@gmail.com>
2026-06-01 00:12:48 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,36 @@ import streamlit as st
 from supabase import create_client
 from datetime import datetime
 
+# ---------------- LOGIN ----------------
+
+USERNAME = "admin"
+PASSWORD = "1234"
+
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if not st.session_state.logged_in:
+
+    st.set_page_config(
+        page_title="Dr. Tag Eye Clinic Login",
+        page_icon="🔐",
+        layout="centered"
+    )
+
+    st.title("🔐 Dr. Tag Eye Clinic Login")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if username == USERNAME and password == PASSWORD:
+            st.session_state.logged_in = True
+            st.rerun()
+        else:
+            st.error("Invalid username or password")
+
+    st.stop()
+
 # ---------------- PAGE CONFIG ----------------
 
 st.set_page_config(
